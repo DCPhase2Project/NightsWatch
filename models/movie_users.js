096e65308fdf7972f6be9d8f1f67487355ca41ff
sarahnash <sarahnash1112@gmail.com>
2019-01-12 12:44:07 -0600

    removed extra index.js file, queries from query.js working

--- models/movie_users.js
+++ models/movie_users.js
@@ -1,14 +1,11 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const movie_users = sequelize.define('movie_users', {
     user_id: DataTypes.INTEGER,
     movie_id: DataTypes.INTEGER
-  }, {});
-  movie_users.associate = function(models) {
+  }, {})
+  movie_users.associate = function (models) {
 
-
-
-
-  };
-  return movie_users;
+  }
+  return movie_users
 };
--- server.js
+++ server.js
@@ -2,22 +2,16 @@ var express = require('express')
 var bodyParser = require('body-parser')
 
 var app = express()
-var db = require('./models')
+var db = require('./models/db')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(__dirname + '/public'))
 
-// Sequelize
-// const Sequelize = require('sequelize')
-// const sequelize = new Sequelize('database')
-
 // why did a 'get' not work here???
 app.post('/send/data', function (request, response, nextFn) {
   let movieSearch = request.body.searchData
   console.log(movieSearch)
-
-  // var movieDB = sequelize.define('movieDB',)
 })
 
 db.movies.findAll()
